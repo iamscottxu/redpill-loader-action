f6fe7803a0fd084ab5cab038b2ffd5f0bbd0af0a
scottxu <xyc0714@gmail.com>
2022-02-12 21:23:19 +0800

    Update build-ds3615xs-7.0.sh

--- build-ds3615xs-7.0.sh
+++ build-ds3615xs-7.0.sh
@@ -9,8 +9,8 @@ mkdir output
 cd DS3615xs-7.0
 
 # download redpill
-git clone --depth=1 https://github.com/RedPill-TTG/redpill-lkm.git
-git clone --depth=1 https://github.com/RedPill-TTG/redpill-load.git
+git clone --depth=1 https://github.com/jumkey/redpill-lkm.git
+git clone --depth=1 https://github.com/jumkey/redpill-load.git
 
 # download syno toolkit
 curl --location "https://sourceforge.net/projects/dsgpl/files/toolkit/DSM7.0/ds.bromolow-7.0.dev.txz/download" --output ds.bromolow-7.0.dev.txz
